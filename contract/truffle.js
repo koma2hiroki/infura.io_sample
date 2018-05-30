eba935795ba743bd38d567febc1e11bf3dc48d38
komatsu_hiroki <koma2hiroki@gmail.com>
2018-05-30 15:00:27 +0900

    不要な情報を削除

--- contract/truffle.js
+++ contract/truffle.js
@@ -3,12 +3,6 @@ const config = require("../config");
 
 module.exports = {
   networks: {
-    ganache: { // Ganache
-      host: "localhost",
-      port: 7545,
-      gas: 4700000,
-      network_id: 5777
-    },
     infura: { // infura.io
       provider: function() {
         return new HDWalletProvider(config.account.mnemonic, config.url)
